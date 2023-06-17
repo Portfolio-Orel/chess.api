1f51832580d7a70b877e675ab3b83e3476fc7d86
Orelzman <orelsmail@gmail.com>
2023-06-17 15:07:48 +0300

    - User rating is now fetched according to user_id

--- src/api/rating.js
+++ src/api/rating.js
@@ -3,8 +3,6 @@ const { tables } = require("../common/constants");
 const { calculateRating } = require("../common/rating_calculator");
 const { knex } = require("../common/request_wrapper");
 
-const logger = require("../common/logger");
-
 const axios = require("axios");
 const cheerio = require("cheerio");
 
@@ -20,9 +18,18 @@ const getRating = async (req, context) =>
   runRequest(req, context, async (_, user_id) => {
     const chess_rating_url = process.env.CHESS_RATING_URL;
     // const { player_id } = req.pathParameters;
-    const player_id = "4169";
-    const result = await axios.get(`${chess_rating_url}${player_id}`);
-    const html = result.data;
+    const result = await knex(tables.users)
+    .where({ id: user_id })
+    .join(tables.chess_user_data, `${tables.users}.id`, `${tables.chess_user_data}.user_id`)
+    .select(`${tables.chess_user_data}.player_number`)
+    .first();
+    
+    if(!result) throw Error("User not found");
+
+    const player_number = result?.player_number;
+
+    const chess_rating_result = await axios.get(`${chess_rating_url}${player_number}`);
+    const html = chess_rating_result.data;
     const $ = cheerio.load(html);
     const israeliRatingElement = $(
       '.full-profile .full-block ul li:contains("מד כושר ישראלי")'
